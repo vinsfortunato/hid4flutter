9f5570bd869c0eb5a43d1e98ede3f91df6598d4b
vinsfortunato <vinsfortunato97@gmail.com>
2023-12-27 12:03:53 +0100

    Update example app with a list of connected devices.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,65 +1,99 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:hid4flutter/hid4flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion = 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    List<HidDevice> devices = await Hid.getDevices();
-
-    print(devices);
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true),
+      home: const DeviceListScreen(),
+    );
+  }
+}
+
+class DeviceListScreen extends StatefulWidget {
+  const DeviceListScreen({super.key});
+
+  @override
+  DeviceListScreenState createState() => DeviceListScreenState();
+}
+
+class DeviceListScreenState extends State<DeviceListScreen> {
+  List<HidDevice> devices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadConnectedDevices();
+  }
+
+  Future<void> _loadConnectedDevices() async {
+    try {
+      List<HidDevice> connectedDevices = await Hid.getDevices();
+      setState(() {
+        devices = connectedDevices;
+      });
+    } catch (e) {
+      // ignore: avoid_print
+      print('Error getting connected devices: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Connected Devices'),
       ),
+      body: _buildDeviceList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+          _loadConnectedDevices(),
+        },
+        tooltip: 'Refresh',
+        child: const Icon(Icons.refresh),
+      ),
+    );
+  }
+
+  Widget _buildDeviceList() {
+    if (devices.isEmpty) {
+      return const Center(
+        child: Text('No connected devices'),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: devices.length,
+      itemBuilder: (context, index) {
+        HidDevice device = devices[index];
+        return Card(
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: ListTile(
+            title: Text('Device $index'),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Vendor ID: ${device.vendorId}'),
+                Text('Product ID: ${device.productId}'),
+                Text('Usage Page: ${device.usagePage}'),
+                Text('Usage: ${device.usage}'),
+                Text('Manufacturer: ${device.manufacturer}'),
+                Text('Product Name: ${device.product}')
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
